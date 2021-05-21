6035175077332cd13940936263594bdf6611995b
snehil1930 <snehil.agrawal1930@gmail.com>
2021-05-21 10:46:46 +0530

    static problem resolve

--- src/Assignment_1/Constants.java
+++ src/Assignment_1/Constants.java
@@ -1,11 +1,11 @@
 package Assignment_1;
 
 public class Constants{
-	public final static String quantity = "quantity";
-    public final static String type = "type";
-    public final static String name = "name";
-    public final static String price = "price";
-    public final static String raw = "raw";
-    public final static String imported = "imported";
-    public final static String manufactured = "manufactured";
+	public static final String quantity = "quantity";
+    public static final String type = "type";
+    public static final String name = "name";
+    public static final String price = "price";
+    public static final String raw = "raw";
+    public static final String imported = "imported";
+    public static final String manufactured = "manufactured";
 }
