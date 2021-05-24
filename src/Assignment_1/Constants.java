f13d048aa680798e4770dc48436dd6fa4400f714
snehil1930 <snehil.agrawal1930@gmail.com>
2021-05-24 17:01:46 +0530

    done with all changes

--- src/Assignment_1/Constants.java
+++ src/Assignment_1/Constants.java
@@ -13,12 +13,18 @@ public class Constants{
     public static final String type = "type";
     public static final String name = "name";
     public static final String price = "price";
+
 //    constants of type in item
+
     public static final String raw = "raw";
     public static final String imported = "imported";
     public static final String manufactured = "manufactured";
+
 //    constants of 100
+
     public static final float hun=100.0f;
+
 //    contsants "-" before paramter of item;
+
     public static final String dash="-";
 }
--- test/Assig1/valdatortester.java
+++ test/Assig1/valdatortester.java
@@ -13,7 +13,10 @@ import org.junit.jupiter.api.Test;
 class valdatortester {
 
 	@Test
-	void validateTest() throws Exception {
+	/*
+	* This test method is used to check whether correct input is passing test or not
+	*/
+	void devTest() throws Exception {
 		
 		String [] args = new String[]{"-name" , "snehil" , "-type" , "raw" , "-price" , "123" , "-quantity" , "2"} ;
 		Validator vc=new Validator();
@@ -25,66 +28,94 @@ class valdatortester {
         assertEquals(it.getPrice(), 123) ;
         assertEquals(it.getName() , "snehil") ;
         assertEquals(it.getQuantity(), 2) ;
-        
-//        Exception and Invalid tests
-        
-        assertThrows(Exception.class , ()->{
-            String[] temp = new String[]{"-type" , "raw" , "-name" , "snehil"} ;
-            new Validator().getDetails(temp) ;
-        });
-        assertThrows(Exception.class,()->{
-        	String[] temp = new String[]{"-name" , "snehil","-type","exported"} ;
-        	new Validator().getDetails(temp);
-        });
-        assertThrows(Exception.class,()->{
-        	String[] temp = new String[]{"-name" , "snehil","-type","exported","-price","-34"} ;
-        	new Validator().getDetails(temp);
-        });
-        assertThrows(Exception.class,()->{
-        	String[] temp = new String[]{"-name" , "snehil","type","exported","price","34","quantity","-3"} ;
-        	new Validator().getDetails(temp);
-        });
-        
-        
-        assertThrows(Exception.class,()->{
-        	String[] temp = new String[]{"-name" , "snehil","-price","34"} ;
-        	new Validator().getDetails(temp);
-        });
-        
-        assertThrows(Exception.class,()->{
-        	String[] temp = new String[]{"-name" , "snehil","-type","imported","-price","34"} ;
-        	new Validator().getDetails(temp);
-        });
-        
-//        testing for last price
-        args = new String[]{"-name" , "snehil" , "-type" , "raw" , "-price" , "123" , "-quantity" , "2"} ;
-        vc=new Validator();
+	}
+
+	/*
+	*This method is use to test inputs on various invalid input and test exceptions
+	 */
+
+	@Test
+	void exceptionsTest() throw Exception
+	{
+
+//		this checks name paramter should be alway come first
+
+		assertThrows(Exception.class , ()->{
+			String[] temp = new String[]{"-type" , "raw" , "-name" , "snehil"} ;
+			new Validator().getDetails(temp) ;
+		});
+
+//		This is use to throw error when option of type doesnot match
+
+		assertThrows(Exception.class,()->{
+			String[] temp = new String[]{"-name" , "snehil","-type","exported"} ;
+			new Validator().getDetails(temp);
+		});
+
+//		this is invalid input as price<0
+
+		assertThrows(Exception.class,()->{
+			String[] temp = new String[]{"-name" , "snehil","-type","exported","-price","-34"} ;
+			new Validator().getDetails(temp);
+		});
+
+//		this is invalid as quantity<0
+		assertThrows(Exception.class,()->{
+			String[] temp = new String[]{"-name" , "snehil","type","exported","price","34","quantity","-3"} ;
+			new Validator().getDetails(temp);
+		});
+
+//		this is invalid as type is compulsory paramter and it is missing
+		assertThrows(Exception.class,()->{
+			String[] temp = new String[]{"-name" , "snehil","-price","34"} ;
+			new Validator().getDetails(temp);
+		});
+
+//		this is valid input
+
+		assertThrows(Exception.class,()->{
+			String[] temp = new String[]{"-name" , "snehil","-type","imported","-price","34"} ;
+			new Validator().getDetails(temp);
+		});
+	}
+
+	/*
+	* this method is tested afer valid and invalid inputs is tested
+	* it test the finalprice of item after calculation
+	*/
+
+	@Test
+	void finalPriceTest()
+	{
+//		it test results for "raw" item
+
+		args = new String[]{"-name" , "snehil" , "-type" , "raw" , "-price" , "123" , "-quantity" , "2"} ;
+		vc=new Validator();
 		it=vc.getDetails(args);
 		it.totaling();
 		float actual=it.getfinalPrice();
-//		System.out.println(actual);
 		float expected=(float)276.75;
 		assertEquals(actual,expected);
-		
+
+//		it test for imported item
+
 		args = new String[]{"-name" , "snehil" , "-type" , "imported" , "-price" , "123" , "-quantity" , "2"} ;
-        vc=new Validator();
+		vc=new Validator();
 		it=vc.getDetails(args);
 		it.totaling();
 		actual=it.getfinalPrice();
-//		System.out.println(actual);
-	    expected=(float)280.6;
+		expected=(float)280.6;
 		assertEquals(actual,expected);
-		
-		
+
+//		it test for manufactured item type
+
 		args = new String[]{"-name" , "snehil" , "-type" , "manufactured" , "-price" , "123" , "-quantity" , "2"} ;
-        vc=new Validator();
+		vc=new Validator();
 		it=vc.getDetails(args);
 		it.totaling();
 		actual=it.getfinalPrice();
-//		System.out.println(actual);
-	    expected=(float)281.66998;
+		expected=(float)281.66998;
 		assertEquals(actual,expected);
-		
 	}
 
 }
